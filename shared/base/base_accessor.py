ecfaaf3ea98a163d5e5f45c1d7edf54490eaf809
Seregax <Serega-x5trf@yandex.ru>
2026-03-17 16:58:26 +0300

    feat: implement shared library

--- shared/base/base_accessor.py
+++ shared/base/base_accessor.py
@@ -1,1 +1,19 @@
-# Implemented in feature/2-shared-library
+from __future__ import annotations
+
+import typing
+
+if typing.TYPE_CHECKING:
+    from aiohttp.web import Application
+
+
+class BaseAccessor:
+    def __init__(self, app: Application) -> None:
+        self.app = app
+        app.on_startup.append(self.connect)
+        app.on_cleanup.append(self.disconnect)
+
+    async def connect(self, app: Application) -> None:
+        pass
+
+    async def disconnect(self, app: Application) -> None:
+        pass
